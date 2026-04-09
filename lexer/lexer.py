b9503b6fc3f4bd664d524b46206188b6dfc7e8e4
Alejandro <alejandrorubalcavav@gmail.com>
2026-04-08 21:16:12 -0600

    Added lexer implementation for syntax highlighting

---
+++ lexer/lexer.py
@@ -0,0 +1,76 @@
+SYMBOL, NUMBER, BOOLEAN, STRING, PARENTHESIS, ERROR = range(6)
+
+def getToken(text, i):
+    while i < len(text) and text[i].isspace():
+        i += 1
+        
+    if i >= len(text):
+        return None, None, i
+        
+    c = text[i]
+
+    # Symbol
+    if c.islower():
+        start = i
+        while i < len(text) and text[i].islower():
+            i += 1
+        return SYMBOL, text[start:i], i
+    
+    # Number
+    elif c.isdigit():
+        start = i
+        while i < len(text) and text[i].isdigit():
+            i += 1
+        return NUMBER, text[start:i], i
+    
+    # Boolean
+    elif c == "#":
+        if i+1 < len(text) and text[i+1] in ["t", "f"]:
+            return BOOLEAN, text[i:i+2], i+2
+        else:
+            return ERROR, text[i], i+1
+        
+    # String
+    elif c == '"':
+        i += 1 # Skip first "
+        start = i
+
+        while i < len(text) and text[i] != '"':
+            if not (text.islower() or text.isdigit() or text[i] == " "):
+                return ERROR, text[i], i+1
+            i += 1
+
+        if i >= len(text):
+            return ERROR, "String not closed", i
+        
+        return STRING, text[start:i], i+1 # Consumes last "
+
+    # Parenthesis
+    elif c in "()":
+        return PARENTHESIS, c, i+1
+    
+    # Error
+    else:
+        return ERROR, c, i+1
+    
+def lexer(text):
+    i = 0
+    tokens = []
+
+    while i < len(text):
+        type, lexeme, i = getToken(text, i)
+
+        if type is None:
+            break
+
+        if type == ERROR:
+            print("Lexical Error")
+            return
+
+        tokens.append((type, lexeme))
+
+    return tokens
+
+textInput = input("Ingresar Texto: ")
+tokens = lexer(textInput)
+print(tokens)
